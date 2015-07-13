139ddfba265ecdf0c5d436475d3db3b04cedba66
Neil Okamoto <neil.okamoto@gmail.com>
2015-07-12 22:30:59 -0700

    make script run cleanly on OS X

--- release/start-driver.sh
+++ release/start-driver.sh
@@ -29,6 +29,11 @@ SERVICE_NAME=driver
 VERSION=`cat VERSION`
 OSGI_BUNDLES="cosbench-log_${VERSION} cosbench-tomcat_${VERSION} cosbench-config_${VERSION} cosbench-http_${VERSION} cosbench-cdmi-util_${VERSION} cosbench-core_${VERSION} cosbench-core-web_${VERSION} cosbench-api_${VERSION} cosbench-mock_${VERSION} cosbench-ampli_${VERSION} cosbench-swift_${VERSION} cosbench-keystone_${VERSION} cosbench-httpauth_${VERSION} cosbench-s3_${VERSION} cosbench-librados_${VERSION} cosbench-scality_${VERSION} cosbench-cdmi-swift_${VERSION} cosbench-cdmi-base_${VERSION} cosbench-driver_${VERSION} cosbench-driver-web_${VERSION}"
 
+if [ `uname` == "Darwin" ]; then
+	SED = "sed -i .bak"
+else
+	SED = "sed -i"
+fi
 
 rm -f ip-port.list
 for i in $(seq 1 $num)
@@ -43,15 +48,15 @@ do
 	cp driver_template.conf driver_$i.conf
 	name="$ip:$x"
 	url="http:\/\/$ip:$x\/driver"
-	sed -i "s/^name=.*$/name=${name}/" driver_$i.conf
-	sed -i "s/^url=.*$/url=${url}/" driver_$i.conf
+	$SED "s/^name=.*$/name=${name}/" driver_$i.conf
+	$SED "s/^url=.*$/url=${url}/" driver_$i.conf
 	ln -s driver_$i.conf driver.conf
 
 	#make driver-tomcat-server.xml
 	rm -f driver-tomcat-server.xml
 	rm -f driver-tomcat-server_$i.xml
 	cp driver-tomcat-server_template.xml driver-tomcat-server_$i.xml
-	sed -i "s/\(.*Connector port=\"\)[^\"]*\(.*\)/\1${x}\2/" driver-tomcat-server_$i.xml 
+	$SED "s/\(.*Connector port=\"\)[^\"]*\(.*\)/\1${x}\2/" driver-tomcat-server_$i.xml 
 	let "x=$x+1"
 	ln -s driver-tomcat-server_$i.xml driver-tomcat-server.xml
 	cd ../
@@ -61,7 +66,6 @@ do
 	#ip/port list
 	echo "$name" >> ip-port.list
 done
-
 
 
 
